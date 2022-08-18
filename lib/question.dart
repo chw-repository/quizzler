d88e0cc3364b239a8b732c069e2528c1803f8370
chw-repository <chw.repository@gmail.com>
2022-08-18 10:34:49 +0900

    end of quiz

--- lib/question.dart
+++ lib/question.dart
@@ -2,10 +2,10 @@ class Question {
   late String questionText;
   late bool questionAnswer;
 
-  // Question(this.questionText, this.questionAnswer);
+  Question(this.questionText, this.questionAnswer);
   // Question({required String q, required bool a}) {
-  Question(String q, bool a) {
-    questionText = q;
-    questionAnswer = a;
-  }
+  // Question(String q, bool a) {
+  //   questionText = q;
+  //   questionAnswer = a;
+  // }
 }
